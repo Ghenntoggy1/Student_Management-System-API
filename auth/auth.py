666c888ee8cf48cff3faa709e5ac0674cd3d78e0
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-05-19 21:15:40 +0300

    feat(responses): Add Generic Response to return. Add validation of JWT based on Signature, Time Expiration. Add HTTPExceptions in case of errors.

--- auth/auth.py
+++ auth/auth.py
@@ -1,11 +1,9 @@
-from email.policy import default
-
 import jwt
 import os
 from dotenv import load_dotenv
 from fastapi.security import OAuth2PasswordBearer
 from datetime import timedelta, timezone, datetime
-from enums.enums import RolesEnum
+from enums.enums import JWTValidationResultsEnum
 
 load_dotenv()
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
@@ -17,24 +15,31 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
+        issue_time = datetime.now(timezone.utc)
+        expire = issue_time + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=JWT_EXPIRY_TIME_MINUTES)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(payload=to_encode, key=JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+        issue_time = datetime.now(timezone.utc)
+        expire = issue_time + timedelta(minutes=JWT_EXPIRY_TIME_MINUTES)
+    to_encode.update({"iat": issue_time, "exp": expire})
+    encoded_jwt = jwt.encode(payload=to_encode,
+                             key=JWT_SECRET_KEY,
+                             algorithm=JWT_ALGORITHM)
     return encoded_jwt
 
 def decode_access_token(token: str):
-    # try:
-    #     decoded_token = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
-    #     return decoded_token
-    # except jwt.ExpiredSignatureError:
-    #     return {"error": "Token has expired"}
-    # except jwt.InvalidTokenError:
-    #     return {"error": "Invalid token"}
-
-    decoded_token = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
-    return decoded_token
+    try:
+        decoded_token = jwt.decode(jwt=token,
+                                   key=JWT_SECRET_KEY,
+                                   algorithms=[JWT_ALGORITHM])
+        return decoded_token
+    except jwt.ExpiredSignatureError:
+        return JWTValidationResultsEnum.is_expired
+    except jwt.InvalidTokenError:
+        return JWTValidationResultsEnum.is_invalid
+    except jwt.InvalidSignatureError:
+        return JWTValidationResultsEnum.invalid_signature
+    except Exception as e:
+        return "UNKNOWN_ERROR"
 
 def verify_password(plain_password, hashed_password):
     return hashed_password == plain_password
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from db.db import SessionLocal
 from services import service
-from db import schemas
+from db.schemas import GenericResponse, UserLogin, UserRequest, Token
 import auth.auth as auth_layer
-from enums.enums import RolesEnum
+from enums.enums import RolesEnum, JWTValidationResultsEnum
 
 app = FastAPI()
 
@@ -20,28 +20,56 @@ def get_database_session():
 async def root():
     return {"message": "Welcome to Student Management System!"}
 
-@app.post("/login/", response_model=schemas.Token)
-async def login(user: schemas.UserLogin, db: Session = Depends(get_database_session)):
+@app.post(path="/login/",
+          response_model=GenericResponse)
+async def login(user: UserLogin, db: Session = Depends(get_database_session)):
     user_obj = service.get_user_by_email(email=user.email, db=db)
-    if not user_obj:
-        return {"message": "Invalid credentials"}
+    if not user_obj or not auth_layer.verify_password(user.password, user_obj.Password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials.")
 
-    if not auth_layer.verify_password(user.password, user_obj.Password):
-        return {"message": "Invalid credentials"}
+    access_token = auth_layer.create_access_token(
+        data={
+            "sub": user_obj.Email,
+            "name": user_obj.FirstName + " " + user_obj.LastName,
+            "role": user_obj.Role,
+        }
+    )
 
-    access_token = auth_layer.create_access_token(data={"sub": user_obj.Email, "role": user_obj.Role})
-    return schemas.Token(access_token=access_token, token_type="bearer")
+    response = GenericResponse(
+        status_code=status.HTTP_200_OK,
+        message="Login successful. Token is issued",
+        data={
+            "access_token": access_token,
+            "token_type": "bearer"
+        }
+    )
+    return response
 
-@app.get("/users/", response_model=list[schemas.UserResponse])
+@app.get("/users/", response_model=GenericResponse)
 async def get_all_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_database_session)):
     users = service.get_all_users(skip=skip, limit=limit, db=db)
-    return users
 
-@app.post("/add_user/", response_model=schemas.UserResponse)
-async def add_user(user: schemas.UserRequest, jwt_token: schemas.Token, db: Session = Depends(get_database_session)):
-    decoded_token: dict = auth_layer.decode_access_token(token=jwt_token.access_token)
-    jwt_token_role: schemas.RolesEnum = decoded_token.get("role")
+    response = GenericResponse(
+        status_code=status.HTTP_200_OK,
+        message="Users retrieved successfully",
+        data=users
+    )
+    return response
+
+@app.post("/add_user/", response_model=GenericResponse)
+async def add_user(user: UserRequest, jwt_token: Token, db: Session = Depends(get_database_session)):
+    decoded_token = service.jwt_validation_response(jwt_token)
+    jwt_token_role: RolesEnum = decoded_token.get("role")
     if RolesEnum.admin != jwt_token_role:
-        return {"message": "You don't have permission to add a user"}
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You do not have required permission to perform this action."
+        )
+    db_user = service.add_user(db=db, user=user)
+    response = GenericResponse(
+        status_code=status.HTTP_201_CREATED,
+        message="User added successfully.",
+        data=db_user
+    )
+    return response
 
-    return service.add_user(db=db, user=user)
