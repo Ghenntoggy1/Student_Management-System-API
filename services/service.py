acbd0754c4cc76709f26a1e152e72d27200f4739
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-05-19 14:01:47 +0300

    feat(endpoints): Add Endpoint for User Creation

--- services/service.py
+++ services/service.py
@@ -1,6 +1,25 @@
-from db import models
+from db import models, schemas
+import bcrypt
 
 from sqlalchemy.orm import Session
 
 def get_all_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
+
+def add_user(db: Session, user: schemas.UserRequest):
+    salt = bcrypt.gensalt()
+    password_bytes = user.password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(password_bytes, salt).decode('utf-8')
+
+    db_user = models.User(
+        Role=user.role,
+        FirstName=user.first_name,
+        LastName=user.last_name,
+        Email=user.email,
+        Password=hashed_password
+    )
+    print(db_user)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
